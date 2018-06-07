7d826a84a3910c20a3b0c5ec8336bfb9fe4922f2
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2018-06-07 08:05:39 +0800

    Update Hive_performance_analysis.sh

--- Hive_performance_analysis.sh
+++ Hive_performance_analysis.sh
@@ -85,77 +85,83 @@ awk '
   split(date_time, stop_time, ",")
 }
 END {
-    print "Log start time : " start_time[1]
-    print "Log end time : "   stop_time[1]
-    print "Last query parsed at : "  last_query_time
-
-    print "\n====Top 3 beeline users===="
+    print "========================================="
+    print "Log_start_time : " start_time[1]
+    print "Log_end_time : "   stop_time[1]
+    print "Last_query_parsed_at : "  last_query_time
+    print "\nTotal_Number_of_Queries_Parsed : " number_of_queries_total
+    print "Total_number_of_Beeline_sessions : " number_of_beeline_sessions
+    print "Total_Number_of_Metastore_hits : " total_metastore_hits
+    print "========================================="
+    print "\nTop_3_beeline_users"
+    print "========================================="
     cntr=0;
     PROCINFO["sorted_in"] = "@val_num_desc"
     for (b_user in beeline_sessions_by_user)
       if(cntr<3){
-        printf b_user " : "beeline_sessions_by_user[b_user]"  "
+        print b_user " : "beeline_sessions_by_user[b_user]
         cntr++
         }
-    print " "
-    print "Total number of Beeline sessions : " number_of_beeline_sessions
 
-
-    print "\n====Top 3 metastore users===="
+    print "========================================="
+    print "\nTop_3_metastore_users"
+    print "========================================="
     user_cntr=0
     PROCINFO["sorted_in"] = "@val_num_desc"
     for (m_user in metastore_hits_user)
       if(user_cntr<3){
-        printf m_user " : "metastore_hits_user[m_user]"  "
+        print m_user " : "metastore_hits_user[m_user]
         user_cntr++
         }
-    print " "
 
-    print "\n====Top 3 metastore commands===="
+    print "========================================="
+    print "\nTop_3_metastore_commands"
+    print "========================================="
     cmd_cntr=0
     PROCINFO["sorted_in"] = "@val_num_desc"
     for (m_cmd in metastore_hits_command)
       if(cmd_cntr<3){
-        printf m_cmd " : "metastore_hits_command[m_cmd]"  "
+        print m_cmd " : "metastore_hits_command[m_cmd]
         cmd_cntr++
         }
 
-    print " "
-    print "Total Number of Metastore hits : " total_metastore_hits
-    print "\nNumber of Total Queries : " number_of_queries_total
-    print "\n====Total time spent in each method, Min, Max, Mean of time spent per query per method====\n"
+    print "========================================="
+    print "\nTime_spent_per_phase_of_execution"
+    print "Method_name Total Min Max Average"
+    print "========================================="
 		print "Driver.run " duration_per_method["Driver.run"] min_duration_per_method["Driver.run"],max_duration_per_method["Driver.run"],\
 		 duration_per_method["Driver.run"]/count_per_method["Driver.run"]
-		print "  Driver.execute " duration_per_method["Driver.execute"] min_duration_per_method["Driver.execute"],max_duration_per_method["Driver.execute"],\
+		print "|=Driver.execute " duration_per_method["Driver.execute"] min_duration_per_method["Driver.execute"],max_duration_per_method["Driver.execute"],\
 		  duration_per_method["Driver.execute"]/count_per_method["Driver.execute"]
-		print "    compile " duration_per_method["compile"] min_duration_per_method["compile"],max_duration_per_method["compile"],\
+		print "|==compile " duration_per_method["compile"] min_duration_per_method["compile"],max_duration_per_method["compile"],\
 		  duration_per_method["compile"]/count_per_method["compile"]
-		print "      parse " duration_per_method["parse"] min_duration_per_method["parse"],max_duration_per_method["parse"],\
+		print "|====parse " duration_per_method["parse"] min_duration_per_method["parse"],max_duration_per_method["parse"],\
 		  duration_per_method["parse"]/count_per_method["parse"]
-		print "      semanticAnalyze " duration_per_method["semanticAnalyze"] min_duration_per_method["semanticAnalyze"],max_duration_per_method["semanticAnalyze"],\
+		print "|====semanticAnalyze " duration_per_method["semanticAnalyze"] min_duration_per_method["semanticAnalyze"],max_duration_per_method["semanticAnalyze"],\
 		  duration_per_method["semanticAnalyze"]/count_per_method["semanticAnalyze"]
-		print "        partition-retrieving " duration_per_method["partition-retrieving"] min_duration_per_method["partition-retrieving"],max_duration_per_method["partition-retrieving"],\
+		print "|======partition-retrieving " duration_per_method["partition-retrieving"] min_duration_per_method["partition-retrieving"],max_duration_per_method["partition-retrieving"],\
 		  duration_per_method["partition-retrieving"]/count_per_method["partition-retrieving"]
-		print "        getInputSummary " duration_per_method["getInputSummary"] min_duration_per_method["getInputSummary"],max_duration_per_method["getInputSummary"],\
+		print "|======getInputSummary " duration_per_method["getInputSummary"] min_duration_per_method["getInputSummary"],max_duration_per_method["getInputSummary"],\
 		  duration_per_method["getInputSummary"]/count_per_method["getInputSummary"]
-		print "        doAuthorization " duration_per_method["doAuthorization"] min_duration_per_method["doAuthorization"],max_duration_per_method["doAuthorization"],\
+		print "|======doAuthorization " duration_per_method["doAuthorization"] min_duration_per_method["doAuthorization"],max_duration_per_method["doAuthorization"],\
 		  duration_per_method["doAuthorization"]/count_per_method["doAuthorization"]
-		print "        serializePlan " duration_per_method["serializePlan"] min_duration_per_method["serializePlan"],max_duration_per_method["serializePlan"],\
+		print "|======serializePlan " duration_per_method["serializePlan"] min_duration_per_method["serializePlan"],max_duration_per_method["serializePlan"],\
 		  duration_per_method["serializePlan"]/count_per_method["serializePlan"]
-		print "        deserializePlan " duration_per_method["deserializePlan"] min_duration_per_method["deserializePlan"],max_duration_per_method["deserializePlan"],\
+		print "|======deserializePlan " duration_per_method["deserializePlan"] min_duration_per_method["deserializePlan"],max_duration_per_method["deserializePlan"],\
 		  duration_per_method["deserializePlan"]/count_per_method["deserializePlan"]
-		print "     releaseLocks " duration_per_method["releaseLocks"] min_duration_per_method["releaseLocks"],max_duration_per_method["releaseLocks"],\
+		print "|===releaseLocks " duration_per_method["releaseLocks"] min_duration_per_method["releaseLocks"],max_duration_per_method["releaseLocks"],\
 		  duration_per_method["releaseLocks"]/count_per_method["releaseLocks"]
-		print "     PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook " duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"] min_duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],max_duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],\
+		print "|==PreHook.o.a.h.h.ql.h.ATSHook " duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"] min_duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],max_duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],\
 		  duration_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"]/count_per_method["PreHook.org.apache.hadoop.hive.ql.hooks.ATSHook"]
-		print "     PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook " duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"] min_duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"],max_duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"],\
+		print "|==PreHook.o.a.h.h.ql.s.a.p.DisallowTransformHook " duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"] min_duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"],max_duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"],\
 		  duration_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"]/count_per_method["PreHook.org.apache.hadoop.hive.ql.security.authorization.plugin.DisallowTransformHook"]
-		print "    TimeToSubmit " duration_per_method["TimeToSubmit"] min_duration_per_method["TimeToSubmit"],max_duration_per_method["TimeToSubmit"],\
+		print "|==TimeToSubmit " duration_per_method["TimeToSubmit"] min_duration_per_method["TimeToSubmit"],max_duration_per_method["TimeToSubmit"],\
 		  duration_per_method["TimeToSubmit"]/count_per_method["TimeToSubmit"]
-		print "    runTasks " duration_per_method["runTasks"] min_duration_per_method["runTasks"],max_duration_per_method["runTasks"],\
+		print "|==runTasks " duration_per_method["runTasks"] min_duration_per_method["runTasks"],max_duration_per_method["runTasks"],\
 		  duration_per_method["runTasks"]/count_per_method["runTasks"]
-		print "    PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook " duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"] min_duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],max_duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],\
+		print "|==PostHook.o.a.h.h.ql.hooks.ATSHook " duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"] min_duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],max_duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"],\
 		  duration_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"]/count_per_method["PostHook.org.apache.hadoop.hive.ql.hooks.ATSHook"]
+print "========================================="
 
 }
 ' "$LOGFILE"
